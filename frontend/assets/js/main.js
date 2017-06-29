9dd47404816cc7b9857db889ac63abb7d0aa7ac0
Vlad Biralo <mr.biralo@yandex.ru>
2017-06-30 01:28:50 +0300

    DARK STYLE (this is top😎)

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -15,7 +15,7 @@ Contacts for cooperation:
 
 */
 
-(function($) {
+(function ($) {
 
 	skel.breakpoints({
 		wide: '(min-width: 961px) and (max-width: 1880px)',
@@ -25,105 +25,109 @@ Contacts for cooperation:
 		mobile: '(max-width: 736px)'
 	});
 
-	$(function() {
+	$(".hover").mouseleave(function () {
+		$(this).removeClass("hover");
+	});
 
-		var	$window = $(window),
+	$(function () {
+
+		var $window = $(window),
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		$window.on('load', function () {
+			$body.removeClass('is-loading');
+		});
 
 		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+		if (skel.vars.IEVersion < 9)
+			$(':last-child').addClass('last-child');
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+		skel.on('+mobile -mobile', function () {
+			$.prioritize(
+				'.important\\28 mobile\\29',
+				skel.breakpoint('mobile').active
+			);
+		});
 
 		// Scrolly links.
-			$('.scrolly').scrolly();
+		$('.scrolly').scrolly();
 
 		// Nav.
-			var $nav_a = $('#nav a');
+		var $nav_a = $('#nav a');
 
-			// Scrolly-fy links.
+		// Scrolly-fy links.
+		$nav_a
+			.scrolly()
+			.on('click', function (e) {
+
+				var t = $(this),
+					href = t.attr('href');
+
+				if (href[0] != '#')
+					return;
+
+				e.preventDefault();
+
+				// Clear active and lock scrollzer until scrolling has stopped
 				$nav_a
-					.scrolly()
-					.on('click', function(e) {
+					.removeClass('active')
+					.addClass('scrollzer-locked');
 
-						var t = $(this),
-							href = t.attr('href');
+				// Set this link to active
+				t.addClass('active');
 
-						if (href[0] != '#')
-							return;
+			});
 
-						e.preventDefault();
+		// Initialize scrollzer.
+		var ids = [];
 
-						// Clear active and lock scrollzer until scrolling has stopped
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
+		$nav_a.each(function () {
 
-						// Set this link to active
-							t.addClass('active');
+			var href = $(this).attr('href');
 
-					});
+			if (href[0] != '#')
+				return;
 
-			// Initialize scrollzer.
-				var ids = [];
+			ids.push(href.substring(1));
 
-				$nav_a.each(function() {
+		});
 
-					var href = $(this).attr('href');
-
-					if (href[0] != '#')
-						return;
-
-					ids.push(href.substring(1));
-
-				});
-
-				$.scrollzer(ids, { pad: 200, lastHack: true });
+		$.scrollzer(ids, { pad: 200, lastHack: true });
 
 		// Header (narrower + mobile).
 
-			// Toggle.
-				$(
-					'<div id="headerToggle">' +
-						'<a href="#header" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+		// Toggle.
+		$(
+			'<div id="headerToggle">' +
+			'<a href="#header" class="toggle"></a>' +
+			'</div>'
+		)
+			.appendTo($body);
 
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
+		// Header.
+		$('#header')
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'left',
+				target: $body,
+				visibleClass: 'header-visible'
+			});
 
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#headerToggle, #header, #main')
-						.css('transition', 'none');
+		// Fix: Remove transitions on WP<10 (poor/buggy performance).
+		if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+			$('#headerToggle, #header, #main')
+				.css('transition', 'none');
 
 	});
 
