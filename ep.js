4ba0db5eb75a03413cb2f13ff60671d404ed2fa4
Vlad Biralo <mr.biralo@yandex.ru>
2020-09-05 16:41:24 +0300

    Migrate to Gist

--- ep.js
+++ ep.js
@@ -1,1 +1,1 @@
-Migrate to Gist => https://gist.github.com/VBIralo/5e7cb1c55615403f2de216d357e730dc.js
+Migrate to Gist => https://gist.githubusercontent.com/VBIralo/5e7cb1c55615403f2de216d357e730dc/raw/debfcf959a5998c9a7ece9b09cb2341a8d38e221/ep.js
